24b6e537950be102582e3916fa94fbf08a118c5c
meredithhall716 <meredith.hall.oboe@gmail.com>
2022-05-28 16:58:36 -0400

    first

--- python/pad_array.py
+++ python/pad_array.py
@@ -1,2 +1,12 @@
 #REMEMBER TO PSEUDOCODE
+from unittest.util import _count_diff_hashable
+
+
 def pad(array, min_size, value = None):
+    count = len(array)
+    pad_count = min_size - count
+    while pad_count > 0:
+        array.append(value)
+        pad_count -= 1
+        
+    return array
